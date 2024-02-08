3e5bdad700b07fd7e4ff7902e1d8da025c08b28d
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-02-09 04:47:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "6926010870:AAGIhb-T6qXG3qJAc9TVxLFLpmNSO98q-Oc"
+    API_ID = "20945078"
+    API_HASH = "93f6b8ce4bb0ab61b4c7e42187f2aa64"
+    AUTH_USERS = '1664376941'
